fced928cc426eca4f5b1191549636bc38ece735a
AmitaiB <blickstein@gmail.com>
2015-06-26 19:15:01 -0400

    completes lab, fails tests, debug after Shabbat

--- pickinFruit/FISViewController.m
+++ pickinFruit/FISViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Joe Burgess. All rights reserved.
 //
 
+#define ARC4RANDOM_MAX 0x100000000
 #import "FISViewController.h"
 
 @interface FISViewController ()
@@ -21,7 +22,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.fruitPicker.delegate = self;
-    self.fruitPicker.dataSource = self;
+    self.fruitPicker .dataSource = self;
     self.fruitsArray = @[@"Apple",
                          @"Orange",
                          @"Banana",
@@ -55,8 +56,54 @@
 }
 
 - (IBAction)spin:(id)sender {
+    [self spinMe];
+}
+
+-(void)spinMe {
     for (NSUInteger component = 0; component < 3; component++) {
-        [self.fruitPicker selectRow:2  inComponent:component animated:YES];
+        [self.fruitPicker selectRow:[self randomFloatBetweenNumber:0 andNumber:self.fruitsArray.count] inComponent:component animated:YES];        
+    }
+    
+    [self checkIfWon];
+}
+
+ 
+-(void)checkIfWon
+{
+    NSUInteger firstRow = [self.fruitPicker selectedRowInComponent:0];
+    NSUInteger secondRow = [self.fruitPicker selectedRowInComponent:1];
+    NSUInteger thirdRow = [self.fruitPicker selectedRowInComponent:2];    
+    if (firstRow == secondRow && secondRow == thirdRow) {
+        //JACKPOT
+        
+    } else {
+        //Try again
+        
+    };
+
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Now what?" message:@"" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Re-spin", nil];
+
+    [alert show];
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 0) {
+        //cancel
+        
+    }
+    if (buttonIndex == 1) {
+        //re-spin
+        [self spinMe];
     }
 }
+
+-(CGFloat)randomFloatBetweenNumber:(CGFloat)minRange andNumber:(CGFloat)maxRange
+{
+    return ((float)arc4random() / ARC4RANDOM_MAX) * (maxRange - minRange);
+}
+
+
+
+
 @end
